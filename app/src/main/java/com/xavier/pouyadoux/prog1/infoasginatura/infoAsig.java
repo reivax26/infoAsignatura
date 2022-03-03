0ffbc83b20218e0a0742c630bf3030cd40dbb90c
reivax26 <92803076+reivax26@users.noreply.github.com>
2022-03-03 18:33:55 +0100

    activity main

---
+++ app/src/main/java/com/xavier/pouyadoux/prog1/infoasginatura/infoAsig.java
@@ -0,0 +1,14 @@
+package com.xavier.pouyadoux.prog1.infoasginatura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class infoAsig extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_asig);
+    }
+}
