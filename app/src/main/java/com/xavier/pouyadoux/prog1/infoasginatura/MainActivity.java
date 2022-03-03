91cd6d4683c67ea597680eb3443de27dd3d513aa
reivax26 <92803076+reivax26@users.noreply.github.com>
2022-03-03 18:17:13 +0100

    first commit

---
+++ app/src/main/java/com/xavier/pouyadoux/prog1/infoasginatura/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xavier.pouyadoux.prog1.infoasginatura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
