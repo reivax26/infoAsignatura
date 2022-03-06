a56f555c346b4272b80bf4dbddc4ed71b592a025
reivax26 <92803076+reivax26@users.noreply.github.com>
2022-03-06 22:53:17 +0100

    recyclerview

---
+++ app/src/main/java/com/xavier/pouyadoux/prog1/infoasginatura/AdapterDatos.java
@@ -0,0 +1,52 @@
+package com.xavier.pouyadoux.prog1.infoasginatura;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdapterDatos  extends RecyclerView.Adapter<AdapterDatos.ViewHolderDatos> {
+
+
+    ArrayList<String> listDatos;
+
+    public AdapterDatos(ArrayList<String> listDatos) {
+        this.listDatos = listDatos;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderDatos onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,null,false);
+        return new ViewHolderDatos(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderDatos holder, int position) {
+        holder.asignarDatos(listDatos.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listDatos.size();
+    }
+
+    public class ViewHolderDatos extends RecyclerView.ViewHolder {
+
+        TextView dato;
+
+        public ViewHolderDatos(@NonNull View itemView) {
+            super(itemView);
+            dato=itemView.findViewById(R.id.idDatos);
+        }
+
+        public void asignarDatos(String datos) {
+            dato.setText(datos);
+        }
+    }
+}
--- app/src/main/java/com/xavier/pouyadoux/prog1/infoasginatura/infoAsignatura.java
+++ app/src/main/java/com/xavier/pouyadoux/prog1/infoasginatura/infoAsignatura.java
@@ -1,6 +1,8 @@
 package com.xavier.pouyadoux.prog1.infoasginatura;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.Picture;
@@ -10,8 +12,12 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class infoAsignatura extends AppCompatActivity {
     Bundle datos;
+    ArrayList<String> listDatos;
+    RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,21 @@ public class infoAsignatura extends AppCompatActivity {
 
                 TextView info = findViewById(R.id.infoAsigActivity2);
                 info.setText(R.string.AllInfoAsig1);
+
+                recycler=(RecyclerView) findViewById(R.id.Recyclerid);
+                recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+                listDatos= new ArrayList<String>();
+
+                listDatos.add("- Comprender la evolución histórica del concepto del arte.");
+                listDatos.add("- Conocer los diferentes periodos históricos del arte, los acontecimientos más importantes y  sus artistas más influyentes.");
+                listDatos.add("- Identificar las técnicas, procesos y herramientas más usadas para la creación de obras de carácter pictórico, escultórico o fotográfico.");
+                listDatos.add("- Aplicar las técnicas y métodos asociados a la creación de obras de arte de diversa naturaleza.");
+                listDatos.add("- Diseñar planes que permitan incluir los procesos asociados al arte, para potenciar la salud y el desarrollo individual humano.");
+
+
+                AdapterDatos adapter = new AdapterDatos(listDatos);
+                recycler.setAdapter(adapter);
                 break;
 
             case 2:
@@ -44,6 +65,22 @@ public class infoAsignatura extends AppCompatActivity {
 
                 TextView info2 = findViewById(R.id.infoAsigActivity2);
                 info2.setText(R.string.AllInfoAsig2);
+
+
+                recycler=(RecyclerView) findViewById(R.id.Recyclerid);
+                recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+                listDatos= new ArrayList<String>();
+
+                listDatos.add("- Comprender la aplicación de los cubos en perspectivas en el dibujo constructivo.");
+                listDatos.add("- Asimilar los principios básicos de la geometría y aplicar trazados fundamentales en el plano.");
+                listDatos.add("- Dominar las transformaciones geométricas elementales.");
+                listDatos.add("- Representar cuerpos y espacios simples mediante el uso de la perspectiva y las proporciones de las superficies.");
+                listDatos.add("- Dibujar perspectivas cónicas, a partir de sus representaciones diédricas, interpretando correctamente la posición del punto de vista y los planos.");
+
+
+                AdapterDatos adapter2 = new AdapterDatos(listDatos);
+                recycler.setAdapter(adapter2);
                 break;
             case 3:
                 titulo = findViewById(R.id.Titulo);
@@ -54,6 +91,21 @@ public class infoAsignatura extends AppCompatActivity {
 
                 TextView info3 = findViewById(R.id.infoAsigActivity2);
                 info3.setText(R.string.AllInfoAsig1);
+
+                recycler=(RecyclerView) findViewById(R.id.Recyclerid);
+                recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+                listDatos= new ArrayList<String>();
+
+                listDatos.add("- Entender los principios de la computación y su evolución a través del tiempo.");
+                listDatos.add("- Reconocer los principales componentes de un computador.");
+                listDatos.add("- Comprender la complejidad de los sistemas operativos.");
+                listDatos.add("- Dominar las funcionalidades, herramientas y procesos del escritorio de Windows.");
+                listDatos.add("- Conocer los campos de aplicación y la naturaleza del software Excel.");
+
+
+                AdapterDatos adapter3 = new AdapterDatos(listDatos);
+                recycler.setAdapter(adapter3);
                 break;
         }
 
